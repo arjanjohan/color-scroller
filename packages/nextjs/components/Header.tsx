ce35898f969c783569126f40c716d4df4d37ec37
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-09-25 17:36:41 +0200

    header update

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -3,9 +3,53 @@
 import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
+
+type HeaderMenuLink = {
+  label: string;
+  href: string;
+  icon?: React.ReactNode;
+};
+
+export const menuLinks: HeaderMenuLink[] = [
+  {
+    label: "Color",
+    href: "/",
+  },
+  {
+    label: "Image",
+    href: "/image",
+  },
+];
+
+export const HeaderMenuLinks = () => {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {menuLinks.map(({ label, href, icon }) => {
+        const isActive = pathname === href;
+        return (
+          <li key={href}>
+            <Link
+              href={href}
+              passHref
+              className={`${
+                isActive ? "bg-secondary shadow-md" : ""
+              } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
+            >
+              {icon}
+              <span>{label}</span>
+            </Link>
+          </li>
+        );
+      })}
+    </>
+  );
+};
 
 /**
  * Site header
@@ -39,12 +83,13 @@ export const Header = () => {
                 setIsDrawerOpen(false);
               }}
             >
+              <HeaderMenuLinks />
             </ul>
           )}
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo_small.png" />
+          <Image alt="Logo" className="cursor-pointer" fill src="/logo_small.png" />
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">Color Scroller</span>
@@ -52,6 +97,7 @@ export const Header = () => {
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
+          <HeaderMenuLinks />
         </ul>
       </div>
       <div className="navbar-end flex-grow mr-4">
